74847beaf579d11ec35db9f3dc7af971860d2ebc
kohei <3173010@stu.hus.ac.jp>
2019-12-27 00:24:59 +0900

    create signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the New Sample App!"
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def edit
@@ -22,5 +30,10 @@ class UsersController < ApplicationController
   def destroy
 
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 
 end
